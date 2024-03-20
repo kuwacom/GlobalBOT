7eeb102ac1b06283bb710483bebfeb120155aab0
kuwacom <kuwa.com3@gmail.com>
2024-03-21 03:17:47 +0900

    update: DBのシステムをアップデート

--- src/utiles/dbManager.ts
+++ src/utiles/dbManager.ts
@@ -5,6 +5,11 @@ import { GBANDB, GChatDB, ServerDB } from "../types/db";
 import env from "../config/env";
 import logger from "./logger";
 
+const serverDBPath = env.db.serverDB;
+const GBANDBPath = env.db.GBANDB;
+const GChatDBPath = env.db.GChatDB;
+const botDBPath = env.db.botDB;
+
 namespace DBManager {
 
     export const botDB = {
@@ -22,7 +27,7 @@ namespace DBManager {
 
 
     export const initialize = async(): Promise<void> => {
-        fs.readdir(env.path.serverDB, (error, files) => { // serverDB
+        fs.readdir(serverDBPath, (error, files) => { // serverDB
             if (error) logger.error("dbManager-serverDB" + error);
             files.filter((file) => {
                 return path.extname(file).toLowerCase() == '.json';
@@ -31,14 +36,14 @@ namespace DBManager {
                 
                 try {
                     const serverId = path.basename(dir, ".json");
-                    serverDBs[serverId] = JSON.parse(fs.readFileSync(env.path.serverDB +"/"+ dir).toString());
+                    serverDBs[serverId] = JSON.parse(fs.readFileSync(serverDBPath +"/"+ dir).toString());
                 } catch (error) {
                     logger.error("ERROR DB:" + dir + "\n" + error);
                 }
             });
         });
 
-        fs.readdir(env.path.GBANDB, (error, files) => { // GBAN DB
+        fs.readdir(GBANDBPath, (error, files) => { // GBAN DB
             if (error) logger.error("dbManager-GBANDB" + error);
             files.filter((file) => {
                 return path.extname(file).toLowerCase() == '.json';
@@ -47,14 +52,14 @@ namespace DBManager {
                 
                 try {
                     const userId = path.basename(dir, ".json");
-                    GBANDBs[userId] = JSON.parse(fs.readFileSync(env.path.GBANDB +"/"+ dir).toString());
+                    GBANDBs[userId] = JSON.parse(fs.readFileSync(GBANDBPath +"/"+ dir).toString());
                 } catch (error) {
                     logger.error("ERROR DB:" + dir + "\n" + error);
                 }
             });
         });
 
-        fs.readdir(env.path.GChatDB, (error, files) => { // GChat DB
+        fs.readdir(GChatDBPath, (error, files) => { // GChat DB
             if (error) logger.error("dbManager-GChatDB" + error);
             files.filter((file) => {
                 return path.extname(file).toLowerCase() == '.json';
@@ -63,7 +68,7 @@ namespace DBManager {
                 
                 try {
                     const channelId = path.basename(dir, ".json");
-                    GChatDBs[channelId] = JSON.parse(fs.readFileSync(env.path.GChatDB +"/"+ dir).toString());
+                    GChatDBs[channelId] = JSON.parse(fs.readFileSync(GChatDBPath +"/"+ dir).toString());
                 } catch (error) {
                     logger.error("ERROR DB:" + dir + "\n" + error);
                 }
@@ -73,7 +78,7 @@ namespace DBManager {
         { // BOTDB
             logger.info("load BOTDB");
             try {
-                const botDB_ = JSON.parse(fs.readFileSync(env.path.botDB).toString());
+                const botDB_ = JSON.parse(fs.readFileSync(botDBPath).toString());
                 for (const key in botDB_) {
                     // @ts-ignore
                     botDB[key] = botDB_[key];
@@ -90,7 +95,7 @@ namespace DBManager {
 
 
     export const saveBotDB = (): boolean => {
-        fs.writeFile(env.path.botDB, JSON.stringify(botDB), async (error) => {
+        fs.writeFile(botDBPath, JSON.stringify(botDB), async (error) => {
             if (error) logger.error("[DBManager.saveBotDB] :" + error);
             logger.debug("DONE Save botDB");
         });
@@ -113,12 +118,26 @@ namespace DBManager {
         };
         return serverDBs[guildId];
     }
-    export const saveServerDB = async(guildId: string, serverDB: ServerDB | undefined = undefined): Promise<void> => {
-        if (serverDB) serverDBs[guildId] = serverDB;
-        fs.writeFile(env.path.serverDB+ "/" + guildId + ".json", JSON.stringify(serverDBs[guildId]), async (error) => {
+    export const saveServerDB = async(guildId: string, serverDB: ServerDB | {} | undefined = undefined): Promise<void> => {
+        if (!fs.existsSync(serverDBPath)) fs.mkdirSync(serverDBPath);
+        if (serverDB == undefined) {
+            fs.writeFile(serverDBPath+ "/" + guildId + ".json", JSON.stringify(serverDBs[guildId]), async (error) => {
             if (error) logger.error("[DBManager.saveServerDB] :" + error);
-            logger.debug("DONE Save ServerDB " + guildId);
-        });
+                logger.debug("DONE Save ServerDB " + guildId);
+            });
+        } else if (Object.keys(serverDB).length == 0) {
+            delete serverDBs[guildId];
+            fs.writeFile(serverDBPath+ "/" + guildId + ".json", JSON.stringify({}), async (error) => {
+                if (error) logger.error("[DBManager.saveServerDB] :" + error);
+                logger.debug("DONE Save ServerDB " + guildId);
+            });
+        } else {
+            serverDBs[guildId] = serverDB as ServerDB;
+            fs.writeFile(serverDBPath+ "/" + guildId + ".json", JSON.stringify(serverDB), async (error) => {
+                if (error) logger.error("[DBManager.saveServerDB] :" + error);
+                logger.debug("DONE Save ServerDB " + guildId);
+            });
+        }
     }
 
 
@@ -126,17 +145,31 @@ namespace DBManager {
         if (!GBANDBs[userId]) return null;
         return GBANDBs[userId];
     }
-    export const saveGBANDB = async(userId: string, GBANDB: GBANDB | undefined = undefined): Promise<void> => {
-        if (GBANDB) GBANDBs[userId] = GBANDB;
-        fs.writeFile(env.path.GBANDB+ "/" + userId + ".json", JSON.stringify(GBANDBs[userId]), async (error) => {
+    export const saveGBANDB = async(userId: string, GBANDB: GBANDB | {} | undefined = undefined): Promise<void> => {
+        if (!fs.existsSync(GBANDBPath)) fs.mkdirSync(GBANDBPath);
+        if (GBANDB == undefined) {
+            fs.writeFile(GBANDBPath + "/" + userId + ".json", JSON.stringify(GBANDBs[userId]), async (error) => {
             if (error) logger.error("[DBManager.saveGBANDB] :" + error);
-            logger.debug("DONE Save GBANDB " + userId);
-        });
+                logger.debug("DONE Save GBANDB " + userId);
+            });
+        } else if (Object.keys(GBANDB).length == 0) {
+            delete GBANDBs[userId];
+            fs.writeFile(GBANDBPath + "/" + userId + ".json", JSON.stringify({}), async (error) => {
+                if (error) logger.error("[DBManager.saveGBANDB] :" + error);
+                logger.debug("DONE Save GBANDB " + userId);
+            });
+        } else {
+            GBANDBs[userId] = GBANDB as GBANDB;
+            fs.writeFile(GBANDBPath + "/" + userId + ".json", JSON.stringify(GBANDB), async (error) => {
+                if (error) logger.error("[DBManager.saveGBANDB] :" + error);
+                logger.debug("DONE Save GBANDB " + userId);
+            });
+        }
     }
     export const removeGBANDB = (userId: string): boolean => {
         if (!GBANDBs[userId]) return false;
         delete GBANDBs[userId];
-        fs.unlink(env.path.GBANDB+ "/" + userId + ".json", async (error) => {
+        fs.unlink(GBANDBPath + "/" + userId + ".json", async (error) => {
             if (error) logger.error("[DBManager.removeGBANDB] :" + error);
             logger.debug("DONE Remove GBANDB " + userId);
         });
@@ -149,17 +182,31 @@ namespace DBManager {
         if (!GChatDBs[channelId]) return null;
         return GChatDBs[channelId];
     }
-    export const saveGChatDB = async(channelId: string, GChatDB: GChatDB | undefined = undefined): Promise<void> => {
-        if (GChatDB) GChatDBs[channelId] = GChatDB;
-        fs.writeFile(env.path.GChatDB+ "/" + channelId + ".json", JSON.stringify(GChatDBs[channelId]), async (error) => {
+    export const saveGChatDB = async(channelId: string, GChatDB: GChatDB | {} | undefined = undefined): Promise<void> => {
+        if (!fs.existsSync(GChatDBPath)) fs.mkdirSync(GChatDBPath);
+        if (GChatDB == undefined) {
+            fs.writeFile(GChatDBPath + "/" + channelId + ".json", JSON.stringify(GChatDBs[channelId]), async (error) => {
             if (error) logger.error("[DBManager.saveGChatDB] :" + error);
-            logger.debug("DONE Save GChatDB " + channelId);
-        });
+                logger.debug("DONE Save GChatDB " + channelId);
+            });
+        } else if (Object.keys(GChatDB).length == 0) {
+            delete GChatDBs[channelId];
+            fs.writeFile(GChatDBPath + "/" + channelId + ".json", JSON.stringify({}), async (error) => {
+                if (error) logger.error("[DBManager.saveGChatDB] :" + error);
+                logger.debug("DONE Save GChatDB " + channelId);
+            });
+        } else {
+            GChatDBs[channelId] = GChatDB as GChatDB;
+            fs.writeFile(GChatDBPath + "/" + channelId + ".json", JSON.stringify(GChatDB), async (error) => {
+                if (error) logger.error("[DBManager.saveGChatDB] :" + error);
+                logger.debug("DONE Save GChatDB " + channelId);
+            });
+        }
     }
     export const removeGChatDB = (channelId: string): boolean => {
         if (!GChatDBs[channelId]) return false;
         delete GChatDBs[channelId];
-        fs.unlink(env.path.GChatDB+ "/" + channelId + ".json", async (error) => {
+        fs.unlink(GChatDBPath + "/" + channelId + ".json", async (error) => {
             if (error) logger.error("[DBManager.removeGChatDB] :" + error);
             logger.debug("DONE Remove GChatDB " + channelId);
         });
